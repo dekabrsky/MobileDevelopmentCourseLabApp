4dec838063ad871d047e7b532a47f42a80125b48
Denis Matafonov <dmatafonov2000@mail.ru>
2024-01-30 20:12:59 +0500

    task 1 - Article

--- app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/presentation/view/article/ArticleFragment.kt
+++ app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/presentation/view/article/ArticleFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.mobiledevelopmentcourselabapp.R
 import com.example.mobiledevelopmentcourselabapp.databinding.FragmentArticleBinding
 
 class ArticleFragment : Fragment() {
@@ -20,7 +23,16 @@ class ArticleFragment : Fragment() {
     ): View {
         _binding = FragmentArticleBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
         // Обращайся к элементам View здесь
+
+        context?.let {
+            Glide
+                .with(it)
+                .load("https://img.championat.com/s/732x488/news/big/b/g/stal-izvesten-novyj-kandidat-na-zamenu-kloppu-v-liverpule_17065467721853904716.jpg")
+                .placeholder(AppCompatResources.getDrawable(it, R.drawable.photo))
+                .into(binding.mainPhoto)
+        }
 
         return root
     }
