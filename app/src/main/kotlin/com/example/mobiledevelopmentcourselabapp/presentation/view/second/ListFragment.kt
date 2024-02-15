9ffeef9a34bed9baa54f1d6b0508df7c59bf6887
Denis Matafonov <dmatafonov2000@mail.ru>
2024-02-15 19:16:32 +0500

    Практика 3 Карточка

--- app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/presentation/view/second/ListFragment.kt
+++ app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/presentation/view/second/ListFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import com.example.mobiledevelopmentcourselabapp.R
 import com.example.mobiledevelopmentcourselabapp.databinding.FragmentListBinding
 import com.example.mobiledevelopmentcourselabapp.presentation.view.list.generator.Generator
 import com.example.mobiledevelopmentcourselabapp.presentation.view.second.adapter.PlayersAdapter
 import com.example.mobiledevelopmentcourselabapp.presentation.view.second.decorator.VerticalSpaceItemDecorator
+import com.example.mobiledevelopmentcourselabapp.presentation.view.second.model.PlayerUiModel
 import com.example.mobiledevelopmentcourselabapp.utils.dpToPx
 import com.example.mobiledevelopmentcourselabapp.utils.orZero
 
@@ -20,7 +24,7 @@ class ListFragment : Fragment() {
 
     private val binding get() = _binding!!
 
-    private val adapter by lazy { PlayersAdapter() }
+    private val adapter by lazy { PlayersAdapter(::onPlayerClicked) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,9 +42,14 @@ class ListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.playersList.adapter = adapter
         adapter.updateItems(Generator.generate())
-        val dividerItemDecoration = DividerItemDecoration(context, VERTICAL)
-        binding.playersList.addItemDecoration(dividerItemDecoration)
-        binding.playersList.addItemDecoration(VerticalSpaceItemDecorator(context?.dpToPx(60).orZero()))
+//        val dividerItemDecoration = DividerItemDecoration(context, VERTICAL)
+//        binding.playersList.addItemDecoration(dividerItemDecoration)
+//        binding.playersList.addItemDecoration(VerticalSpaceItemDecorator(context?.dpToPx(60).orZero()))
+    }
+
+    private fun onPlayerClicked(player: PlayerUiModel) {
+        val bundle = bundleOf(CardFragment.CARD_PLAYER_KEY to player)
+        view?.findNavController()?.navigate(R.id.action_navigation_list_to_cardFragment, bundle)
     }
 
     override fun onDestroyView() {
